a98beaa53addd1ae1dc2a6cd1962f5674229d091
Your Name <you@example.com>
2024-11-22 15:51:13 +0800

    init1

---
+++ README.md
@@ -0,0 +1,2 @@
+npm install cloudinary cors dotenv express mongoose multer react-router-dom
+npm init -y
---
+++ back/utils/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+    cloud_name: "dweij4vwl",
+    api_key: "",
+    api_secret: "",
+});
+
+module.exports = cloudinary;
