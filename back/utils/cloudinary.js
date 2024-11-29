88131142ab92df8c321db254566cdd048e9c0b66
Your Name <you@example.com>
2024-11-29 14:53:10 +0800

    init

--- back/utils/cloudinary.js
+++ back/utils/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
     cloud_name: "dweij4vwl",
-    api_key: "",
-    api_secret: "",
+    api_key: "565234542576734",
+    api_secret: "285lf5MswNpWnZftOvRRKT-q-ak",
 });
 
 module.exports = cloudinary;
