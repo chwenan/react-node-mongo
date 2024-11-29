47b6029958a5ae11b6bd853fa340d22207e06913
chwenan <105399621+chwenan@users.noreply.github.com>
2024-11-29 15:51:39 +0800

    Update cloudinary.js
    delete

--- back/utils/cloudinary.js
+++ back/utils/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: "dweij4vwl",
-    api_key: "565234542576734",
-    api_secret: "285lf5MswNpWnZftOvRRKT-q-ak",
+    cloud_name: "",
+    api_key: "",
+    api_secret: "",
 });
 
 module.exports = cloudinary;
